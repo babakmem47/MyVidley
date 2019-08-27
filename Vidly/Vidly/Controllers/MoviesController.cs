9cfd334733b36fdf3ae35d6f5651f7f72da62ffe
babak35102 <babak35102@gmail.com>
2019-08-27 16:26:25 +0430

    ActionParameters

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -14,7 +15,23 @@ namespace Vidly.Controllers
             //return HttpContext("Hello World");
             //return HttpNotFound();        => //return 404 page error result
             //return new EmptyResult();     => //return empty page
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(int? pageindex, string sortBy)
+        {
+            if (!pageindex.HasValue)
+                pageindex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageindex, sortBy));
         }
     }
 }
